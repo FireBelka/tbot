aad9dca88cb75b4dcbff56f37dbfc39875610f58
Belko Danila <danila.belko.ext@nokia.com>
2023-05-27 14:44:48 +0200

    inline message

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -4,9 +4,12 @@ from telebot import types
 def any_user(message: types.Message, bot: TeleBot):
 
     # keyboard
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Flat")
-    item2 = types.KeyboardButton("Cottage")
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    item1 = types.InlineKeyboardButton("Flat", callback_data='flat')
+    item2 = types.InlineKeyboardButton("Cottage", callback_data='cottage')
     markup.add(item1, item2)
     bot.send_message(message.chat.id, "Hi, {0.first_name}!\nI'm - <b>{1.first_name}</b>, bot.\nWhich type of house do you prefer".format(message.from_user, bot.get_me()),
         parse_mode='html', reply_markup=markup)
+
+
+    
