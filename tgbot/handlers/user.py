42228ce319e4250f263b308d3a1d3388b9c020fb
Belko Danila <danila.belko.ext@nokia.com>
2023-05-27 14:35:00 +0200

    added feature

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -1,8 +1,12 @@
 from telebot import TeleBot
-from telebot.types import Message
+from telebot import types
 
-def any_user(message: Message, bot: TeleBot):
-    """
-    You can create a function and use parameter pass_bot.
-    """
-    bot.send_message(message.chat.id, "Hello, user!")
+def any_user(message: types.Message, bot: TeleBot):
+
+    # keyboard
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Flat")
+    item2 = types.KeyboardButton("Cottage")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, "Hi, {0.first_name}!\nI'm - <b>{1.first_name}</b>, bot.\nWhich type of house do you prefer".format(message.from_user, bot.get_me()),
+        parse_mode='html', reply_markup=markup)
