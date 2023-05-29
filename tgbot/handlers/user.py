477ff0d74e28ee814679b0bfce48e146e7061cc3
Vladislav Markovsky <vladislav.markovsky.ext@nokia.com>
2023-05-29 20:58:06 +0200

    added range to Minsk

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -6,6 +6,7 @@ choise =["","",""]
 houses = ["house", "summer", "cottage"]
 costs=["15","15-25","25-50","50-100", "100-300", "300+"]
 directions=["moscow","logo","madzel","hrodno","brest","dzer","slyck","other"]
+range=["15km","30km","60km"]
 
 def any_user(message: types.Message, bot: TeleBot):
 
@@ -48,7 +49,15 @@ def callback_inline(call, bot: TeleBot):
                 bot.send_message(call.message.chat.id, 'Замечательно, осталось выбрать направление', reply_markup=markup)
             elif call.data in directions:  
                 choise[2]=str(call.data)
-                bot.send_message(call.message.chat.id, "Ваш выбор:\n{0} \n{1} \n{2}".format(choise[0],choise[1],choise[2])) 
+                markup = types.InlineKeyboardMarkup(row_width=3)
+                item1 = types.InlineKeyboardButton("До 15км", callback_data=directions[0])
+                item2 = types.InlineKeyboardButton("До 30км", callback_data=directions[1])
+                item3 = types.InlineKeyboardButton("До 60км", callback_data=directions[2])
+                markup.add(item1,item2,item3)
+                bot.send_message(call.message.chat.id, 'Осталось чуть-чуть, какое расстояние от Минска для вас предпочтительно', reply_markup=markup)
+            elif call.data in range:
+                choise[3]=str(call.data)
+                bot.send_message(call.message.chat.id, "Ваш выбор:\n{0} \n{1} \n{2} \n{3}".format(choise[0],choise[1],choise[2],choise[3])) 
             else :             
                 bot.send_message(call.message.chat.id, 'Alart! Alart! Palestine is under attack!')              
  
