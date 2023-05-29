aaea97920350bb2799f05e9bd6e1cbec8f422699
Belko Danila <danila.belko.ext@nokia.com>
2023-05-29 20:36:01 +0200

    update name resolution

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -5,7 +5,7 @@ from telebot import types
 choise =["","",""]
 houses = ["house", "summer", "cottage"]
 costs=["15","15-25","25-50","50-100", "100-300", "300+"]
-directions=["moscow","logo","madzel","hrodno","brest","h15dzer","slyck","other"]
+directions=["moscow","logo","madzel","hrodno","brest","dzer","slyck","other"]
 
 def any_user(message: types.Message, bot: TeleBot):
 
@@ -23,7 +23,7 @@ def callback_inline(call, bot: TeleBot):
     try:
         if call.message:
             if call.data in houses:
-                choise[0]=call.data
+                choise[0]=str(call.data)
                 markup = types.InlineKeyboardMarkup(row_width=3)
                 item1 = types.InlineKeyboardButton("До 15.000$", callback_data=costs[0])
                 item2 = types.InlineKeyboardButton("15-25.000$", callback_data=costs[1])
@@ -34,7 +34,7 @@ def callback_inline(call, bot: TeleBot):
                 markup.add(item1,item2,item3,item4,item5,item6)
                 bot.send_message(call.message.chat.id, 'Отлично, теперь давайте определимся с бюджетом', reply_markup=markup)             
             elif call.data in costs:
-                choise[1]=call.data
+                choise[1]=str(call.data)
                 markup = types.InlineKeyboardMarkup(row_width=3)
                 item1 = types.InlineKeyboardButton("Московское", callback_data=directions[0])
                 item2 = types.InlineKeyboardButton("Логойское", callback_data=directions[1])
@@ -47,7 +47,7 @@ def callback_inline(call, bot: TeleBot):
                 markup.add(item1,item2,item3,item4,item5,item6)
                 bot.send_message(call.message.chat.id, 'Замечательно, осталось выбрать направление', reply_markup=markup)
             elif call.data in directions:  
-                choise[2]=call.data
+                choise[2]=str(call.data)
                 bot.send_message(call.message.chat.id, "Ваш выбор:\n{0} \n{1} \n{2}".format(choise[0],choise[1],choise[2])) 
             else :             
                 bot.send_message(call.message.chat.id, 'Alart! Alart! Palestine is under attack!')              
